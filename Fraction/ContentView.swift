bf725b774a0d29cf1bfddaa583b6bb63bbbb4d5b
Dewa Bisma <idewagedebisma@gmail.com>
2023-03-29 12:10:15 +0700

    Initial Commit

---
+++ Fraction/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fraction
+//
+//  Created by Bisma Mahendra I Dewa Gede on 29/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fraction/FractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FractionApp.swift
+//  Fraction
+//
+//  Created by Bisma Mahendra I Dewa Gede on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
