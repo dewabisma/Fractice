efcd17e38b746f4f326779d494a7a7954938ead6
Dewa Bisma <idewagedebisma@gmail.com>
2023-04-03 10:10:34 +0700

    done algo generate pair of fraction

--- Fractice/ConvertDecimalToFraction.swift
+++ Fractice/ConvertDecimalToFraction.swift
@@ -7,9 +7,7 @@
 
 import Foundation
 
-typealias Rational = (numerator : Int, denominator : Int)
-
-func convertDecimalToFraction(decimalVal : Double, withPrecision eps : Double = 1.0E-6) -> Rational {
+func convertDecimalToFraction(decimalVal : Double, withPrecision eps : Double = 1.0E-6) -> Fraction {
     var x = decimalVal
     var a = floor(x)
     var (h1, k1, h, k) = (1, 0, Int(a), 1)
@@ -19,5 +17,8 @@ func convertDecimalToFraction(decimalVal : Double, withPrecision eps : Double = 
         a = floor(x)
         (h1, k1, h, k) = (h, k, h1 + Int(a) * h, k1 + Int(a) * k)
     }
-    return (h, k)
+    
+    let deci = convertFractionToDecimal(numerator: h, denominator: k)
+    
+    return (h, k, deci)
 }
--- Fractice/GenerateFraction.swift
+++ Fractice/GenerateFraction.swift
@@ -7,17 +7,45 @@
 
 import Foundation
 
-func generateFraction(maxVal:Int = 10) -> (numerator:Int, denominator:Int) {
+typealias Fraction = (numerator:Int, denominator:Int, decimal: Double)
+typealias FractionPair = (f1:Fraction, f2: Fraction)
+
+func convertFractionToDecimal(numerator:Int, denominator:Int) -> Double {
+    let nume = Double(numerator)
+    let deno = Double(denominator)
+    
+    return nume / deno
+}
+
+func generateFraction(maxVal:Int = 10) -> Fraction {
     let numerator = Int.random(in: 1..<maxVal)
     var denominator:Int
     
     if numerator == maxVal - 1 {
         denominator = maxVal
         
-        return (numerator:numerator, denominator:denominator)
+        let deci = convertFractionToDecimal(numerator: numerator, denominator: denominator)
+        
+        return (numerator:numerator, denominator:denominator, decimal: deci)
     }
     
     denominator = Int.random(in: numerator+1...maxVal)
     
-    return (numerator:numerator, denominator:denominator)
+    let deci = convertFractionToDecimal(numerator: numerator, denominator: denominator)
+    
+    return (numerator:numerator, denominator:denominator, decimal: deci)
+}
+
+func generateFractionPair(maxVal:Int = 10, includeNegative:Bool = false) -> FractionPair {
+    var fraction1 = generateFraction(maxVal: maxVal)
+    var fraction2 = generateFraction(maxVal: maxVal)
+    
+    if !includeNegative {
+        let sortedFraction = sortFraction(operand: Operand.minus, fraction1: fraction1.decimal, fraction2: fraction2.decimal)
+        
+        fraction1 = convertDecimalToFraction(decimalVal: sortedFraction.f1)
+        fraction2 = convertDecimalToFraction(decimalVal: sortedFraction.f2)
+    }
+    
+    return (fraction1, fraction2)
 }
