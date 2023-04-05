31acee10034d8eab20ba91a0f2b55a0379514c49
Dewa Bisma <idewagedebisma@gmail.com>
2023-04-05 09:27:10 +0700

    done organizing files

--- Fractice/Screens/SettingScreen.swift
+++ Fractice/Screens/SettingScreen.swift
@@ -7,6 +7,33 @@
 
 import SwiftUI
 
+struct ModeSoal:View {
+    @State var vibrate:Bool
+    
+    var body: some View {
+        HStack{
+            Text("Soal Bilangan Pecahan")
+                .font(.system(size:18))
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+                .padding(10)
+            Spacer()
+            
+            Toggle(isOn: $vibrate) {
+                }
+            .frame(maxWidth: 50)
+            .padding(10)
+            
+            
+        }
+        .frame(maxWidth: 327, maxHeight: 68)
+        .background(Color("PurpleLight"))
+        .cornerRadius(20)
+        .shadow(color:Color("PurpleDark"), radius: 0.1, x:0, y:5)
+        .padding(.bottom,40)
+    }
+}
+
 struct SettingScreen: View {
     @State private var vibrateOnBilangan = false
     @State private var vibrateOnCerita = false
@@ -36,26 +63,7 @@ struct SettingScreen: View {
                         .foregroundColor(Color("OrangeDark"))
                         .padding(.bottom,40)
                         
-                    HStack{
-                        Text("Soal Bilangan Pecahan")
-                            .font(.system(size:18))
-                            .fontWeight(.heavy)
-                            .foregroundColor(.white)
-                            .padding(10)
-                        Spacer()
-                        
-                        Toggle(isOn: $vibrateOnBilangan) {
-                            }
-                        .frame(maxWidth: 50)
-                        .padding(10)
-                        
-                        
-                    }
-                    .frame(maxWidth: 327, maxHeight: 68)
-                    .background(Color("PurpleLight"))
-                    .cornerRadius(20)
-                    .shadow(color:Color("PurpleDark"), radius: 0.1, x:0, y:5)
-                    .padding(.bottom,40)
+                    ModeSoal(vibrate: vibrateOnBilangan)
                     
                     HStack{
                         Text("Soal Cerita Pecahan")
